fecc671f2af94ae6a52cbe77c36236b7353f33bc
saeedmaghdam <smasafat@gmail.com>
2023-06-18 01:03:26 +0200

    changed services so they're streaming instead of caching

--- SampleReverseProxy.Client2/Program.cs
+++ SampleReverseProxy.Client2/Program.cs
@@ -1,5 +1,6 @@
 using System.Net.Sockets;
 using System.Net.WebSockets;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,44 +12,58 @@ var app = builder.Build();
 
 app.Use(async (HttpContext context, Func<Task> next) =>
 {
-    if (context.WebSockets.IsWebSocketRequest)
+    //// Forward the request to the TCP server
+    //var client = new TcpClient("localhost", 8001);
+    //var writer = new StreamWriter(client.GetStream());
+    //var reader = new StreamReader(client.GetStream());
+
+    //// Write the request path and body to the TCP stream
+    ////var body = new StreamReader(context.Request.Body).ReadToEnd();
+    //writer.WriteLine(context.Request.Path);
+    //writer.Flush();
+
+    //// Read the response from the TCP server
+    //var response = reader.ReadToEnd();
+
+    //// Write the response to the HTTP response
+    //context.Response.Clear();
+    //context.Response.StatusCode = 200; // Or whatever status code you want.
+    //context.Response.ContentType = "text/html"; // Or whatever content type you want.
+    //await context.Response.WriteAsync(response);
+
+    //// Close the TCP client
+    //client.Close();
+
+
+
+    // Forward the request to the TCP server
+    using var client = new TcpClient("localhost", 8001);
+    using var writer = new StreamWriter(client.GetStream());
+    using var reader = new StreamReader(client.GetStream());
+
+    // Write the request path and body to the TCP stream
+    await writer.WriteLineAsync(context.Request.Path);
+    await writer.FlushAsync();
+
+    // Set the response status code and content type
+    context.Response.StatusCode = 200; // Or whatever status code you want.
+    context.Response.ContentType = "text/html"; // Or whatever content type you want.
+
+    // Get the response stream from the HTTP response
+    var responseStream = context.Response.Body;
+
+    // Read the response from the TCP server and write it directly to the response stream
+    var buffer = new char[4096];
+    int bytesRead;
+    while ((bytesRead = await reader.ReadAsync(buffer, 0, buffer.Length)) > 0)
     {
-        using var clientWebSocket = new ClientWebSocket();
-        var targetUri = new Uri("ws://localhost:3000" + context.Request.Path);
-
-        await clientWebSocket.ConnectAsync(targetUri, context.RequestAborted);
-        using var serverWebSocket = await context.WebSockets.AcceptWebSocketAsync();
-
-        var buffer = new byte[1024 * 4];
-        var serverTask = Redirect(serverWebSocket, clientWebSocket, buffer, context.RequestAborted);
-        var clientTask = Redirect(clientWebSocket, serverWebSocket, buffer, context.RequestAborted);
-
-        await Task.WhenAll(serverTask, clientTask);
+        var bytes = Encoding.UTF8.GetBytes(buffer, 0, bytesRead);
+        await responseStream.WriteAsync(bytes, 0, bytes.Length);
+        await responseStream.FlushAsync();
     }
-    else
-    {
-        // Forward the request to the TCP server
-        var client = new TcpClient("localhost", 8001);
-        var writer = new StreamWriter(client.GetStream());
-        var reader = new StreamReader(client.GetStream());
 
-        // Write the request path and body to the TCP stream
-        //var body = new StreamReader(context.Request.Body).ReadToEnd();
-        writer.WriteLine(context.Request.Path);
-        writer.Flush();
-
-        // Read the response from the TCP server
-        var response = reader.ReadToEnd();
-
-        // Write the response to the HTTP response
-        context.Response.Clear();
-        context.Response.StatusCode = 200; // Or whatever status code you want.
-        context.Response.ContentType = "text/html"; // Or whatever content type you want.
-        await context.Response.WriteAsync(response);
-
-        // Close the TCP client
-        client.Close();
-    }
+    // Close the TCP client
+    client.Close();
 });
 
 app.Run();
--- SampleReverseProxy.Server2/Worker.cs
+++ SampleReverseProxy.Server2/Worker.cs
@@ -12,6 +12,54 @@ namespace SampleReverseProxy.Server2
             _logger = logger;
         }
 
+        //public async Task StartAsync(CancellationToken cancellationToken)
+        //{
+        //    // Create a TCP listener
+        //    var listener = new TcpListener(IPAddress.Parse("127.0.0.1"), 8001);
+        //    listener.Start();
+
+        //    while (true)
+        //    {
+        //        try
+        //        {
+        //            // Accept a TCP client
+        //            var client = listener.AcceptTcpClient();
+        //            var reader = new StreamReader(client.GetStream());
+        //            var writer = new StreamWriter(client.GetStream());
+
+        //            // Read the request from the TCP stream
+        //            var request = reader.ReadLine();
+        //            //var body = reader.ReadLine();
+
+        //            // Split the request into the path and query
+        //            var pathAndQuery = request.Split('?');
+        //            var path = pathAndQuery[0];
+        //            var query = pathAndQuery.Length > 1 ? pathAndQuery[1] : "";
+
+        //            // Create an HTTP client and send the request
+        //            var httpClient = new HttpClient();
+        //            //var httpContent = new StringContent(body);
+        //            //var httpResponse = await httpClient.GetAsync("http://localhost:3000" + path + "?" + query);
+        //            var httpResponse = await httpClient.GetAsync("https://www.google.com/" + path + "?" + query);
+
+        //            // Stream the response from the HTTP client to the TCP client
+        //            await httpResponse.Content.CopyToAsync(client.GetStream());
+        //            //// Read the response and write it to the TCP stream
+        //            //var response = await httpResponse.Content.ReadAsStringAsync();
+        //            //writer.WriteLine(response);
+        //            //writer.Flush();
+
+
+        //            // Close the TCP client
+        //            client.Close();
+        //        }
+        //        catch (Exception ex)
+        //        {
+        //            _logger.LogError(ex.Message, ex);
+        //        }
+        //    }
+        //}
+
         public async Task StartAsync(CancellationToken cancellationToken)
         {
             // Create a TCP listener
@@ -23,13 +71,13 @@ namespace SampleReverseProxy.Server2
                 try
                 {
                     // Accept a TCP client
-                    var client = listener.AcceptTcpClient();
+                    var client = await listener.AcceptTcpClientAsync();
                     var reader = new StreamReader(client.GetStream());
                     var writer = new StreamWriter(client.GetStream());
 
                     // Read the request from the TCP stream
-                    var request = reader.ReadLine();
-                    //var body = reader.ReadLine();
+                    var request = await reader.ReadLineAsync();
+                    //var body = await reader.ReadLineAsync();
 
                     // Split the request into the path and query
                     var pathAndQuery = request.Split('?');
@@ -37,15 +85,16 @@ namespace SampleReverseProxy.Server2
                     var query = pathAndQuery.Length > 1 ? pathAndQuery[1] : "";
 
                     // Create an HTTP client and send the request
-                    var httpClient = new HttpClient();
-                    //var httpContent = new StringContent(body);
-                    //var httpResponse = await httpClient.GetAsync("http://localhost:3000" + path + "?" + query);
-                    var httpResponse = await httpClient.GetAsync("https://www.google.com/" + path + "?" + query);
+                    using (var httpClient = new HttpClient())
+                    {
+                        //var httpContent = new StringContent(body);
+                        //var httpResponse = await httpClient.GetAsync("http://localhost:3000" + path + "?" + query);
+                        var httpResponse = await httpClient.GetAsync("https://www.google.com" + path + "?" + query);
 
-                    // Read the response and write it to the TCP stream
-                    var response = await httpResponse.Content.ReadAsStringAsync();
-                    writer.WriteLine(response);
-                    writer.Flush();
+                        // Stream the response from the HTTP client to the TCP client
+                        var responseStream = await httpResponse.Content.ReadAsStreamAsync();
+                        await responseStream.CopyToAsync(client.GetStream());
+                    }
 
                     // Close the TCP client
                     client.Close();
